96cfc0b8d7b0264dcdd128c37e8ea68ea1ec40ec
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 12:49:34 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/ping", (request, response) => {
+  response.send("are you there?");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
