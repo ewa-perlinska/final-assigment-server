e35213a783b2ec83a3a4f2871811eb5640f273a8
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 20:56:42 +0100

    Add router to show all events

--- src/events/router.js
+++ src/events/router.js
@@ -4,6 +4,16 @@ const Event = require("./model");
 
 const router = new Router();
 
+router.get("/event", async function(request, response, next) {
+  try {
+    const events = await Event.findAll();
+    response.send(events);
+    console.log("done");
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/event", auth, async (request, response) => {
   console.log("how my request looks?", request.user.dataValues.id);
   const newEvent = { ...request.body, userId: request.user.dataValues.id };
