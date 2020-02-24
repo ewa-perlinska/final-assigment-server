4582968fa221307f4cbcad92ba039cdf29e0ebd8
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 13:47:21 +0100

    Set up router

--- src/user/model.js
+++ src/user/model.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = require("../db.js");
+const db = require("../../db");
 
 const User = sequelize("user", {
   username: { type: Sequelize.STRING, allowNull: false },
---
+++ src/user/router.js
@@ -0,0 +1,32 @@
+const { Router } = require("express");
+const router = new Router();
+
+const User = require("./model");
+const bcrypt = require("bcrypt");
+
+const { toJWT } = require("../auth/jwt");
+
+router.post("/signup", async (request, response) => {
+  if (!request.body.email || !request.body.password) {
+    return response
+      .status(400)
+      .send("Missing email or password in request body");
+  }
+  const hashedPassword = bcrypt.hashSync(request.body.password, 10);
+  try {
+    await User.create({
+      ...request.body,
+      password: hashedPassword
+    });
+    response.status(201).send("User created");
+  } catch (error) {
+    console.log(error.name);
+    switch (error.name) {
+      case "SequelizeUniqueConstraintError":
+        return response.status(400).send({ message: "Email not unique" });
+
+      default:
+        return response.status(400).send("Baaaddd request");
+    }
+  }
+});
