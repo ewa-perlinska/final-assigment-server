975c832c616535ad8207a382c667b4c02cb7a426
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 20:50:44 +0100

    Fix request url

--- src/events/router.js
+++ src/events/router.js
@@ -4,7 +4,7 @@ const Event = require("./model");
 
 const router = new Router();
 
-router.post("/events", auth, async (request, response) => {
+router.post("/event", auth, async (request, response) => {
   console.log("how my request looks?", request.user.dataValues.id);
   const newEvent = { ...request.body, userId: request.user.dataValues.id };
   const event = await Event.create(newEvent);
